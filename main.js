aace01e2dd900272202bd405965376b14931210f
3arpbl3y <3arpbl3y@gmail.com>
2022-04-11 22:03:14 +0500

    start project. made canvas.

---
+++ main.js
@@ -0,0 +1,18 @@
+const canvas = document.querySelector("#game");
+const ctx = canvas.getContext("2d");
+
+const game = {
+  cellSize: 30,
+  playField: [],
+  rowCount: 20,
+  columnCount: 10,
+  startGame() {},
+  initPlayField() {
+    const { rowCount, columnCount } = this;
+
+    this.playField = [];
+    for (let row = 0; row < array.length; row++) {
+      const element = array[row];
+    }
+  },
+};
