94affaecd8e5c9f6544362cbf1594679b26b508d
3arpbl3y <3arpbl3y@gmail.com>
2022-04-12 21:51:35 +0500

    done until skilled enoughed to understand code

--- main.js
+++ main.js
@@ -4,8 +4,33 @@ const ctx = canvas.getContext("2d");
 const game = {
   cellSize: 30,
   playField: [],
-  rowCount: 20,
-  columnCount: 10,
+
+  rowCount: 20, //height
+  columnCount: 10, //width
+
+  //figures to play and rotate
+  tetrominos: {
+    I: [[0, 0, 0, 0], [(1, 1, 1, 1)], [(0, 0, 0, 0)], [(0, 0, 0, 0)]],
+    O: [[1, 1], [(1, 1)]],
+    S: [[0, 1, 1], [(1, 1, 0)], [(0, 0, 0)]],
+    Z: [[1, 1, 0], [(0, 1, 1)], [(0, 0, 0)]],
+    L: [[0, 0, 1], [(1, 1, 1)], [(0, 0, 0)]],
+    J: [[1, 0, 0], [(1, 1, 1)], [(0, 0, 0)]],
+    T: [
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0],
+    ],
+  },
+  tetroColor: {
+    I: "cyan",
+    O: "yellow",
+    S: "purple",
+    J: "green",
+    L: "orange",
+    T: "red",
+  },
+  currentSequence: [],
   startGame() {
     this.initPlayField();
   },
@@ -21,6 +46,11 @@ const game = {
       this.playField.push(tempRow);
     }
   },
+  genSequence() {
+ const tetroCount = this.currentSequence.length !== Object.keys(this.tetrominos).length)
+  
+ const randomName = Math.floor(Math.random()* tet)
+  
 };
 
 game.startGame();
