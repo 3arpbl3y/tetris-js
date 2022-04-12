c8cdbf76087144b9a44216471525502ab29be92c
3arpbl3y <3arpbl3y@gmail.com>
2022-04-12 21:19:55 +0500

    made playField

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const canvas = document.querySelector("#game");
+const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 const game = {
@@ -6,13 +6,22 @@ const game = {
   playField: [],
   rowCount: 20,
   columnCount: 10,
-  startGame() {},
+  startGame() {
+    this.initPlayField();
+  },
   initPlayField() {
     const { rowCount, columnCount } = this;
 
     this.playField = [];
-    for (let row = 0; row < array.length; row++) {
-      const element = array[row];
+    for (let row = 0; row < rowCount; row++) {
+      const tempRow = [];
+      for (let col = 0; col < columnCount; col++) {
+        tempRow.push(0);
+      }
+      this.playField.push(tempRow);
     }
   },
 };
+
+game.startGame();
+console.log(game);
